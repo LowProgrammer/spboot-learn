25a811708dd9fcbe63841e9d155e8abc01ad2b58
陈群飞 <1320954152@qq.com>
2019-08-19 19:04:28 +0800

    嘉实基金接口问题排查

---
+++ java-prac/src/main/java/com/java/learn/Test.java
@@ -0,0 +1,16 @@
+package com.java.learn;
+
+/**
+ * @author feifei
+ * @Classname Test
+ * @Description TODO
+ * @Date 2019/8/19 14:31
+ * @Created by 陈群飞
+ */
+public class Test {
+    public static void main(String[] args) {
+        String dat="   1";
+        System.out.println(dat.trim());
+        System.out.println(!"".equals(""));
+    }
+}
